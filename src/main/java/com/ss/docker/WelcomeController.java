0ccc0960a0f73e1f132f07da171c8a7796814a02
Sominath Shelake <30572097+shelke88@users.noreply.github.com>
2026-01-08 01:47:32 +0530

    changes

--- src/main/java/com/ss/docker/WelcomeController.java
+++ src/main/java/com/ss/docker/WelcomeController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class WelcomeController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello(){
         return ResponseEntity.ok("Welcome from Docker container application....");
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health(){
+        return ResponseEntity.ok("Health checks for Docker container application....");
     }
 }
